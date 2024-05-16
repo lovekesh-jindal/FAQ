506b52ec049e29d9b8be2dec646ab030bec9c5db
lovekesh-jindal <lovekeshjindal05@gmail.com>
2024-05-17 01:29:16 +0530

    first commmit

---
+++ src/FAQItem.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+const FAQItem = ({faq , idx}) =>{
+    const [isShow, setIsShow] = useState(false);
+    useEffect(()=>{
+      if(idx === 0){
+        setIsShow(true);
+      }   
+    },[])
+    const handleClick = () =>{
+        setIsShow((isShow)=> !isShow);
+    }
+   return <div className="faq-box">
+   <div className="que" onClick={handleClick}>
+    <button className= {isShow ? 'arrow' :''}>></button>
+    <div>{faq.question}</div>
+   </div>
+  { isShow && <div className='ans'>{faq.answer}</div> }
+   </div>
+
+
+}
+
+export default FAQItem;
